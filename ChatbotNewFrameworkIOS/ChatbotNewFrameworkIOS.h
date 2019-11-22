ad352ad26ca2be2d3b71a87fe874d218692ca50a
Ajeet Sharma <ajeetsharma@Ajeets-Mac-mini.local>
2019-11-22 18:12:37 +0530

    Initial commit

---
+++ ChatbotNewFrameworkIOS/ChatbotNewFrameworkIOS.h
@@ -0,0 +1,19 @@
+//
+//  ChatbotNewFrameworkIOS.h
+//  ChatbotNewFrameworkIOS
+//
+//  Created by Ajeet Sharma on 22/11/19.
+//  Copyright © 2019 Telus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatbotNewFrameworkIOS.
+FOUNDATION_EXPORT double ChatbotNewFrameworkIOSVersionNumber;
+
+//! Project version string for ChatbotNewFrameworkIOS.
+FOUNDATION_EXPORT const unsigned char ChatbotNewFrameworkIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatbotNewFrameworkIOS/PublicHeader.h>
+
+
